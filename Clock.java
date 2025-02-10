4e16e220a161689258a5c069869c062722e620cb
b52-wq <giang.bkc15035@student.bkacad.edu.vn>
2025-02-10 19:43:10 +0700

    Add files via upload

---
+++ Clock.java
@@ -0,0 +1,71 @@
+package com.example.hung_timeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Clock extends AppCompatActivity {
+    private TextView dateYear;
+    private TextView digitalClock;
+    private final Handler handler = new Handler();
+    private Runnable runnable;
+    public ImageButton clock1;
+    public ImageButton clock3;
+    public ImageButton clock4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clock);
+
+        clock1 = findViewById(R.id.Clock1);
+        clock3 = findViewById(R.id.Clock3);
+        clock4 = findViewById(R.id.Clock4);
+        dateYear = findViewById(R.id.dateYear);
+        digitalClock = findViewById(R.id.digitalClock);
+
+        clock1.setOnClickListener(view -> {
+            Intent intent = new Intent(Clock.this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        clock3.setOnClickListener(view -> {
+            Intent intent = new Intent(Clock.this, StopWatch.class);
+            startActivity(intent);
+        });
+
+        clock4.setOnClickListener(view -> {
+            Intent intent = new Intent(Clock.this, CountDown.class);
+            startActivity(intent);
+        });
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                String currentTime = new SimpleDateFormat("HH:mm:ss", Locale.getDefault()).format(new Date());
+                digitalClock.setText(currentTime);
+
+                String currentDate = new SimpleDateFormat("yyyy/MM/dd", Locale.getDefault()).format(new Date());
+                dateYear.setText(currentDate);
+
+                handler.postDelayed(this, 1000);
+            }
+        };
+
+        handler.post(runnable); // Bắt đầu chạy runnable để cập nhật thời gian và ngày
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
+    }
+}
